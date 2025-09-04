1017d52ab5d8a0de5707cbbe9f46edb3212c342f
monsieurcheri <monsieurcheri@gmail.com>
2025-09-05 00:16:07 +1000

    ADD: sound effect to mode toggle

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -4,10 +4,12 @@ import { useState } from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
+import { useSound } from "react-sounds";
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
   const [isDark, setIsDark] = useState(false);
+  const { play } = useSound("ui/input_focus");
 
   function handleClick() {
     if (isDark) {
@@ -20,7 +22,14 @@ export function ModeToggle() {
   }
 
   return (
-    <Button variant="ghost" className="size-8" onClick={handleClick}>
+    <Button
+      variant="ghost"
+      className="size-8"
+      onClick={() => {
+        handleClick();
+        play();
+      }}
+    >
       {isDark ? <Moon className="size-5" /> : <Sun className="size-5" />}
     </Button>
   );
